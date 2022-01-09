7d638380e4ab0a8ae75677e8d07167acab157be2
Elvan H <elvanhd@gmail.com>
2022-01-10 05:45:44 +0700

    using async methods when querying a database

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,8 +1,9 @@
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -18,17 +19,17 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public ActionResult<List<Product>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            List<Product> products = _context.Products.ToList();
+            List<Product> products = await _context.Products.ToListAsync();
 
             return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Product> GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            Product product = _context.Products.Find(id);
+            Product product = await _context.Products.FindAsync(id);
 
             return product == null ? NotFound() : Ok(product);
         }
