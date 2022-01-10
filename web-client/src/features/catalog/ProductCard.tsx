c433aead041c5c586d617a215a6f876303a4b00c
Elvan H <elvanhd@gmail.com>
2022-01-10 15:34:08 +0700

    styling the product card

--- web-client/src/features/catalog/ProductCard.tsx
+++ web-client/src/features/catalog/ProductCard.tsx
@@ -1,8 +1,10 @@
 import {
+  Avatar,
   Button,
   Card,
   CardActions,
   CardContent,
+  CardHeader,
   CardMedia,
   Typography,
 } from '@mui/material';
@@ -15,24 +17,42 @@ interface Props {
 export default function ProductCard({ product }: Props) {
   return (
     <Card>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: 'secondary.main' }}>
+            {product.name.charAt(0)}
+          </Avatar>
+        }
+        title={product.name}
+        titleTypographyProps={{
+          sx: {
+            fontWeight: 'bold',
+            color: 'primary.main',
+          },
+        }}
+      />
       <CardMedia
         component='img'
-        height='200'
         image={product.pictureUrl}
-        alt='green iguana'
+        title={product.name}
+        alt={product.name}
+        sx={{
+          height: '200',
+          backgroundSize: 'cover',
+          bgcolor: 'grey.300',
+        }}
       />
       <CardContent>
-        <Typography gutterBottom variant='h5' component='div'>
-          Lizard
+        <Typography gutterBottom color='secondary' variant='h5'>
+          ${(product.price / 100).toFixed(2)}
         </Typography>
         <Typography variant='body2' color='text.secondary'>
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          {product.brand} / {product.type}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size='small'>Share</Button>
-        <Button size='small'>Learn More</Button>
+        <Button size='small'>Add to Cart</Button>
+        <Button size='small'>View</Button>
       </CardActions>
     </Card>
   );
--- web-client/src/features/catalog/ProductList.tsx
+++ web-client/src/features/catalog/ProductList.tsx
@@ -10,7 +10,7 @@ export default function ProductList({ products }: Props) {
   return (
     <Grid container spacing={4}>
       {products.map((product: Product) => (
-        <Grid item xs={12} md={6} xl={4} key={product.id}>
+        <Grid item xs={6} md={4} xl={3} key={product.id}>
           <ProductCard key={product.id} product={product} />
         </Grid>
       ))}
