18adc730b768a9305d83185ec28c9a44163643b6
Elvan H <elvanhd@gmail.com>
2022-01-10 09:54:33 +0700

    using the useState hook

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,4 +1,6 @@
-const products = [
+import { useState } from 'react';
+
+const DUMMY_PRODUCTS = [
   {
     id: 1,
     name: 'Product 1',
@@ -13,16 +15,26 @@ const products = [
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta cum quibusdam consequatur suscipit! Minus aliquam, molestiae assumenda iste nobis error, ab recusandae ipsa velit repellendus sint esse explicabo. Dolore, saepe.',
   },
-  {
-    id: 3,
-    name: 'Product 3',
-    price: 300,
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta cum quibusdam consequatur suscipit! Minus aliquam, molestiae assumenda iste nobis error, ab recusandae ipsa velit repellendus sint esse explicabo. Dolore, saepe.',
-  },
 ];
 
 function App() {
+  const [products, setProducts] = useState(DUMMY_PRODUCTS);
+
+  function addProduct() {
+    setProducts((prevProducts) => {
+      const newId = prevProducts.length + 1;
+      const newProduct = {
+        id: newId,
+        name: 'Product ' + newId,
+        price: 100 * newId,
+        description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta cum quibusdam consequatur suscipit! Minus aliquam, molestiae assumenda iste nobis error, ab recusandae ipsa velit repellendus sint esse explicabo. Dolore, saepe.',
+      };
+
+      return [...prevProducts, newProduct];
+    });
+  }
+
   return (
     <div className='app'>
       <h1>EcommerceApp</h1>
@@ -36,6 +48,7 @@ function App() {
       >
         This is a sample application for an ecommerce website.
       </p>
+      <button onClick={addProduct}>Add Product</button>
       <ul>
         {products.map((product) => (
           <li key={product.id}>
