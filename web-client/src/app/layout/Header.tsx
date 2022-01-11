f7f61825ce006c40139e16514defa5bd6b6faee2
Elvan H <elvanhd@gmail.com>
2022-01-11 22:32:29 +0700

    styling the nav bar

--- web-client/src/app/layout/Header.tsx
+++ web-client/src/app/layout/Header.tsx
@@ -2,6 +2,7 @@ import { ShoppingCart } from '@mui/icons-material';
 import {
   AppBar,
   Badge,
+  Box,
   IconButton,
   List,
   ListItem,
@@ -17,58 +18,77 @@ interface Props {
 }
 
 const midLinks = [
-  { title: 'catalog', path: '/catalog' },
-  { title: 'about', path: '/about' },
-  { title: 'contact', path: '/contact' },
+  { title: 'Catalog', path: '/catalog' },
+  { title: 'About', path: '/about' },
+  { title: 'Contact', path: '/contact' },
 ];
 
 const rightLinks = [
-  { title: 'login', path: '/login' },
-  { title: 'register', path: '/register' },
+  { title: 'Login', path: '/login' },
+  { title: 'Register', path: '/register' },
 ];
+
+const navStyles = {
+  color: 'inherit',
+  textDecoration: 'none',
+  typography: 'h6',
+  '&:hover': {
+    color: 'grey.500',
+  },
+  '&.active': {
+    color: 'text.secondary',
+  },
+};
 
 export default function Header({ darkMode, handleThemeChange }: Props) {
   return (
     <AppBar position='static' sx={{ mb: 4 }}>
-      <Toolbar>
-        <Typography
-          component={NavLink}
-          variant='h6'
-          to='/'
-          sx={{ color: 'inherit', textDecoration: 'none' }}
-        >
-          ECOMMERCE
-        </Typography>
-        <Switch checked={darkMode} onChange={handleThemeChange} />
+      <Toolbar
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Box display='flex' alignItems='center'>
+          <Typography
+            component={NavLink}
+            variant='h6'
+            to='/'
+            sx={navStyles}
+            exact
+          >
+            ECOMMERCE
+          </Typography>
+          <Switch checked={darkMode} onChange={handleThemeChange} />
+        </Box>
+
         <List sx={{ display: 'flex' }}>
           {midLinks.map(({ title, path }) => (
-            <ListItem
-              component={NavLink}
-              to={path}
-              key={title}
-              sx={{ color: 'inherit', typography: 'h6' }}
-            >
+            <ListItem component={NavLink} to={path} key={title} sx={navStyles}>
               {title.toUpperCase()}
             </ListItem>
           ))}
         </List>
-        <IconButton size='large' color='inherit'>
-          <Badge badgeContent={3} color='secondary'>
-            <ShoppingCart />
-          </Badge>
-        </IconButton>
-        <List sx={{ display: 'flex' }}>
-          {rightLinks.map(({ title, path }) => (
-            <ListItem
-              component={NavLink}
-              to={path}
-              key={title}
-              sx={{ color: 'inherit', typography: 'h6' }}
-            >
-              {title.toUpperCase()}
-            </ListItem>
-          ))}
-        </List>
+        <Box display='flex' alignItems='center'>
+          <IconButton size='large' color='inherit'>
+            <Badge badgeContent={3} color='secondary'>
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+          <List sx={{ display: 'flex' }}>
+            {rightLinks.map(({ title, path }) => (
+              <ListItem
+                component={NavLink}
+                to={path}
+                key={title}
+                sx={navStyles}
+              >
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Toolbar>
     </AppBar>
   );
